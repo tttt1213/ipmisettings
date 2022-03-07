d3053e7ae621c30e1a41570d7cb59a47e9d0bace
tttt1213 <62640993+tttt1213@users.noreply.github.com>
2022-03-07 13:31:06 +0900

    Add files via upload

---
+++ read.py
@@ -0,0 +1,12 @@
+﻿import socket
+
+host = "192.168.10.238"
+port = 8629 #適当なPORTを指定してあげます
+massage = ('192.168.20.95 NA read')
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #オブジェクトの作成をします
+client.connect((host, port)) #これでサーバーに接続します
+client.send(massage.encode()) #適当なデータを送信します（届く側にわかるように）
+response = client.recv(512) #レシーブは適当な2の累乗にします（大きすぎるとダメ）
+
+print(response test)
